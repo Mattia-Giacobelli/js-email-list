ea6d65a7f966150d4600c7a8492d07b487139496
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-23 16:15:09 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+console.log('Email list');
+
+
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+*/
+
+//Save ul DOM element
+
+//Stamp 10 random email as li
+//-create variable with li markup
+//-cycle 10 times to generate the email addresses
+//-Get emails from boolean API
+//-Add email to li
+//-Stamp result in the ul
